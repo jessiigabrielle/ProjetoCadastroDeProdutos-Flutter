03833c3be2a5887e55eb229702f1fd57169ede1c
jessiigabrielle <jessi.gabriellee@gmail.com>
2020-07-22 22:23:43 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+// teste commit
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
